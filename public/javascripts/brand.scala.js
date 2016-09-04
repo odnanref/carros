a10c79d1af971427ef89717d01b69ca179e11e84
Fernando Andre <netriver@gmail.com>
2016-09-04 15:13:42 -0300

    file name change

---
+++ public/javascripts/brand.scala.js
@@ -0,0 +1,59 @@
+/**
+ * Brand and Models javascript code
+ *
+ */
+
+function onBrandChange(model_id) {
+    model_id = model_id || 0;
+    // add models to model select
+    var id = $('#make').val();
+    getModelsForBrand(id, model_id);
+}
+
+function getModelsForBrand(id, model_id) {
+    model_id = model_id || 0;
+    //http_url = "@routes.NotificationController.getModelByBrand(31337)";
+    http_url = "/getmodelbybrand/31337";
+    http_url = http_url.replace("31337", id);
+
+    $("#model").empty();
+
+    $.get( http_url, function( data ) {
+        data.forEach( function (ent) {
+            $("#model").append(getOption(ent));
+        });
+
+        console.log("Olha o modelo .. " + model_id);
+        if ( (model_id+0) > 0) {
+            console.log("Already exists a selection @model.id " + model_id);
+            $('#model').val(model_id);
+        }
+    });
+}
+
+function getBrands() {
+    //http_url = "@routes.NotificationController.getBrands()";
+    http_url = "/getbrands";
+    $("#make").empty();
+
+    $.get( http_url, function( data ) {
+        data.forEach( function (ent) {
+            $("#make").append(getOption(ent));
+        });
+    });
+}
+
+function getModelsForBrandLocal(make_id){
+    $("#model").empty();
+    models.forEach(function (ent) {
+        if (ent.make == make_id ) {
+            $("#model").append(getOption(ent));
+        }
+    });
+}
+
+function getOption(ent) {
+    return "<option value='" + ent.id + "'>" +
+        ent.descricao +
+        "</option>";
+}
