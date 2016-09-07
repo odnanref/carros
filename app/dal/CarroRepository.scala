701dbc824602c17bbfb566d96205e24d29a46ca0
Fernando Andre <netriver@gmail.com>
2016-09-06 21:30:39 -0300

    insert correction

--- app/dal/CarroRepository.scala
+++ app/dal/CarroRepository.scala
@@ -27,9 +27,9 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
   import driver.api._
 
   /**
-   * Here we define the table. It will have a name of people
-   */
-  private class CarroTable(tag: Tag) extends Table[Carro](tag, "carro") {
+    * Here we define the table. It will have a name of people
+    */
+  protected class CarroTable(tag: Tag) extends Table[Carro](tag, "carro") {
 
     /** The ID column, which is the primary key, and auto incremented */
     def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
@@ -54,16 +54,16 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
     def model = column[Long]("model")
 
     /**
-     * This is the tables default "projection".
-     *
-     * It defines how the columns are converted to and from the Person object.
-     *
-     * In this case, we are simply passing the id, name and page parameters to the Person case classes
-     * apply and unapply methods.
-     */ 
+      * This is the tables default "projection".
+      *
+      * It defines how the columns are converted to and from the Person object.
+      *
+      * In this case, we are simply passing the id, name and page parameters to the Person case classes
+      * apply and unapply methods.
+      */
     def * = (id, name, year, description, img, keywords, state, model) <>
       ((Carro.apply _).tupled, Carro.unapply)
-    
+
   }
 
   val PAGESIZE: Int = 10
@@ -75,17 +75,17 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
   }
 
   /**
-   * The starting point for all queries on the people table.
-   */
+    * The starting point for all queries on the people table.
+    */
   private val carro = TableQuery[CarroTable]
 
   /**
-   * Create a car with the given name and age.
-   *
-   * This is an asynchronous operation, it will return a future of the created car, which can be used to obtain the
-   * id for that person.
-   */
-  def create(name: String, description: String, year:Int, img:String, keywords: String, state: String, model:Long) :
+    * Create a car with the given name and age.
+    *
+    * This is an asynchronous operation, it will return a future of the created car, which can be used to obtain the
+    * id for that person.
+    */
+  def create(name: String, description: String, year: Int, img: String, keywords: String, state: String, model: Long):
   Future[Carro] = db.run {
     // We create a projection of just the name and age columns, since we're not inserting a value for the id column
     (carro.map(p => (p.name, p.year, p.description, p.img, p.keywords, p.state, p.model))
@@ -94,13 +94,18 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
       // And we define a transformation for the returned value, which combines our original parameters with the
       // returned id
       into ((nameAge, id) => Carro(id, nameAge._1, nameAge._2, nameAge._3, nameAge._4, nameAge._5, nameAge._6, nameAge._7))
-    // And finally, insert the car into the database
-    ) += (name, year, description, img, keywords, state, model)
+      // And finally, insert the car into the database
+      ) +=(name, year, description, img, keywords, state, model)
   }
 
+  //val insertQuery = carro returning carro.map(_.id) into ((Carro, id) => Carro.copy(id = id))
+  val insertQuery = carro returning carro.map(_.id.get)
+
   /** Insert a new car. */
-  def insert(car: Carro): Future[Unit] =
-    db.run(carro += car).map(_ => ())
+  def insert(car: Carro): Future[Long] = {
+    val action = (insertQuery += car)
+    db.run(action)
+  }
 
   /**
    * Create a car with the given name and age.
@@ -139,7 +144,7 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
     *
     * @param id
     * @return Future[Option[Carro]]
-    */
+    **/
   def get(id: Long): Future[Option[Carro]] = {
     dbConfig.db.run(carro.filter(_.id === id).result.headOption)
   }
@@ -179,5 +184,17 @@ class CarroRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
     */
   def remove(id:Long): Unit = {
     db.run(carro.filter(_.id === id ).delete)
+  }
+
+  /**
+    * Update only the filename of a specific car id
+    *
+    * @param filename
+    * @param car_id
+    * @return
+    */
+  def updateMainImage(filename:String, car_id: Long) :Future[Int] = {
+    val q = for { l <- carro if l.id === car_id } yield l.img
+    db.run(q.update(filename))
   }
 }
